43c6e961d4f371b112dfcfd6a001a9240bd158d0
dazayy00 <zs21020562@estudiantes.uv.mx>
2025-10-24 23:58:35 -0600

    avances

--- Celular/Clases.py
+++ Celular/Clases.py
@@ -4,8 +4,14 @@ class Celular:
         self.modelo = modelo
         self.camara = camara
 
+    def llamar(self):
+        print(f'Estas llamando desde un telefono : {self.modelo}')
+
+    def colgar(self):
+        print(f'Cortaste la llamada desde un telefono {self.modelo}')
+
 celular1 = Celular("Apple", "Iphone 15", "40MP")
 celular2 = Celular("Xiaomi", "Redmi 23", "15MP")
 
-print(celular1.marca)
-print(celular2.marca)
+celular1.llamar()
+celular2.colgar()
